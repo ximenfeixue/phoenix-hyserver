89b3cb306aae52ce55575c7e5c114d47b31a788c
Chen peifeng <chenpeifeng@gintong.com>
2016-11-03 19:28:23 +0800

    cleanup code

--- phoenix-hyserver-provider/src/test/java/com/ginkgocap/ywxt/knowledge/web/test/KnowledgeWebTest.java
+++
@@ -1,9 +0,0 @@
-package com.ginkgocap.ywxt.knowledge.web.test;
-
-
-public class KnowledgeWebTest extends BaseTestCase
-{
-    private final String testTitle = "世界就是数据，一切皆可相连，人与人、人与物、物与物互联互通";
-
-   
-}
